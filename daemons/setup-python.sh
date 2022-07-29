9d10dfbc25c63c10cdbbb94992c3adb1b19e1c45
Abhishek Amralkar <abhishekamralkar@gmail.com>
2022-07-29 16:59:44 +0530

    robo-env: add script to track all python package installation

---
+++ daemons/setup-python.sh
@@ -0,0 +1,44 @@
+#!//bin/bash
+
+# Author: Abhishek Anand Amralkar
+# This script installs Python Packages
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+unset CDPATH
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Pick script location
+SETUP_DIR=$(pwd)
+os_release=$(cat /etc/os-release | awk -F '=' '/^NAME/{print $2}' | awk '{print $1}' | tr -d '"')
+now=$(date +"%d-%m-%Y-%H-%M-%S")
+package='Python'
+
+install_started() {
+    echo "${os_release} ${package} package installation started at ${now}!"
+}
+
+install_completed() {
+    echo "${os_release} ${package} package installation finished at ${now}!"
+}
+
+install_python_packages() {
+    if [ -f /etc/redhat-release ]; then 
+    install_started
+    sudo dnf install python3-devel -y 1> /dev/null
+    install_completed
+    elif [ -f /etc/lsb-release ]; then
+    install_started
+    sudo apt-get install python3-dev -y 1> /dev/null
+    install_completed
+    fi
+
+}
+
+main() {
+    install_python_packages
+}
+
+main
