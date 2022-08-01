d325a4ae3e2becf953c5cf3a3b93de1c99b57fa3
Abhishek Amralkar <abhishekamralkar@gmail.com>
2022-08-01 18:07:15 +0530

    robo-env: update vagrant script

--- daemons/setup-vagrant.sh
+++ daemons/setup-vagrant.sh
@@ -1,42 +1,40 @@
 #!/bin/sh
 # Author: Abhishek Anand Amralkar
 # This script installs Hashicorp Vagrant.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
 unset CDPATH
-#CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Fail on error
-set -e
+source ./helper-func.sh
 
-# Update deps
-sudo apt-get update -y 
-sudo apt-get upgrade -y
+# Pick script location
+SETUP_DIR=$(pwd)
+package=$(get_script_name)
+get_release
+get_date
 
-VAGRANT_VERSION=${VAGRANT_VERSION:-"2.2.4"}
-DIR=${DIR:-"/tmp"}
-echo "Installing Vagrant..."
+install_vagrant() {
+    if [ -f /etc/redhat-release ]; then
+    install_started
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+    sudo yum -y install vagrant
+    install_completed
+    elif [ -f /etc/lsb-release]; then
+    install_started
+    wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update && sudo apt install vagrant -y
+    install_completed
+    fi
+}
 
-# check if vagrant installed or not?
-dpkg -s vagrant &> /dev/null
+main() {
+    install_vagrant
+}
 
-if [ $? -eq 0 ]; then
-    echo "Vagrant is installed!"
-else
-    echo "Vagrant is NOT installed!"
-    cd $DIR && wget --no-check-certificate https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_x86_64.deb 
-    sudo dpkg -i vagrant_${VAGRANT_VERSION}_x86_64.deb
-    echo "Installing required plug-ins"
-    vagrant plugin install vagrant-vbguest
-    vagrant plugin update vagrant-vbguest
-    vagrant plugin install vagrant-triggers
-    echo "Restarting vagrant services..."
-    sudo service vboxdrv restart
-    cd $DIR && sudo rm -rf vagrant_${VAGRANT_VERSION}_x86_64.deb 
-
-fi
-
-echo "---------------------------------"
-echo "Please restart VirtualBox if you are using it"
-echo "Set VAGRANT_HOME if you intend to change the default location for boxes"
-echo "If you see vboxdrv probe errors, please sign the key as mentioned in\nhttp://askubuntu.com/questions/760671/could-not-load-vboxdrv-after-upgrade-to-ubuntu-16-04-and-i-want-to-keep-secu"
-echo "---------------------------------"
-echo "Done!"
+main
