fd5834601efddd8bc9f547aea99d320171e67a72
Abhishek Amralkar <abhishekamralkar@gmail.com>
2022-08-01 18:01:03 +0530

    robo-env: add packer script

--- daemons/setup-packer.sh
+++ daemons/setup-packer.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 # Author: Abhishek Anand Amralkar
 # This script installs Packer
 
@@ -9,21 +8,28 @@ set -o nounset
 
 unset CDPATH
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PKR_BIN=${PKR_BIN:-"/usr/local/bin/packer"}
-URL="https://releases.hashicorp.com/packer"
-PKR_VER="$(curl -sL $URL | grep -v beta | grep -Po "_(\d*\.?){3}" | sed 's/_//' | sort -V | tail -1)"
-ZIP="packer_${PKR_VER}_linux_amd64.zip"
-INSTALL_DIR=${1:-/usr/local/bin/}
+
+source ./helper-func.sh
+
+# Pick script location
+SETUP_DIR=$(pwd)
+package=$(get_script_name)
+get_release
+get_date
 
 install_packer (){
-    if [ -e "${PKR_BIN}" ];
-     then
-        echo "Packer is already installed"   
-    else
-        echo "Installing Packer..."
-        sudo curl -s ${URL}/${PKR_VER}/packer_${PKR_VER}_linux_amd64.zip -o ${INSTALL_DIR}/${ZIP}
-        sudo unzip -o ${INSTALL_DIR}/$ZIP -d $INSTALL_DIR && sudo rm -v ${INSTALL_DIR}/$ZIP
-        echo "Done!"
+    if [ -f /etc/redhat-release ]; then
+    install_started
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+    sudo yum -y install packer
+    install_completed
+    elif [ -f /etc/lsb-release]; then
+    install_started
+    wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update && sudo apt install packer
+    install_completed
     fi
 }
 
