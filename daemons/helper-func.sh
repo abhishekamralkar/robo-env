d0e5037b6693c5503c0bd94441df5285c9a68953
Abhishek Amralkar <abhishekamralkar@gmail.com>
2025-03-25 21:42:43 -0700

    delete alacritty setup amd improve helper function

--- daemons/helper-func.sh
+++ daemons/helper-func.sh
@@ -9,36 +9,44 @@ unset CDPATH
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Pick script location
-SETUP_DIR=$(pwd)
+SETUP_DIR="$(pwd)"
 
 install_started() {
     echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "+ ${os_release}: ${package} package installation started at ${now}!+"
+    echo "+ ${os_release}: ${package} package installation started at ${now}! +"
     echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 }
 
 install_completed() {
     echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "+ ${os_release}: ${package} package installation finished at ${now}!+"
+    echo "+ ${os_release}: ${package} package installation finished at ${now}! +"
     echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 }
 
 get_release() {
-    os_release=$(cat /etc/os-release | awk -F '=' '/^NAME/{print $2}' | awk '{print $1}' | tr -d '"')
+    # Extract OS release name
+    local os_info
+    os_info=$(cat /etc/os-release | awk -F '=' '/^NAME/{print $2}' | awk '{print $1}' | tr -d '"')
+    os_release="${os_info:-Unknown}" # Default to "Unknown" if empty
 }
 
 get_date() {
+    # Get current date and time
     now=$(date +"%d-%m-%Y-%H-%M-%S")
 }
 
 get_script_name() {
-    basename "$0" | awk -F '-' '{print $2}' | cut -f1 -d"."
+    # Extract script name
+    local script_name
+    script_name=$(basename "$0" | awk -F '-' '{print $2}' | cut -f1 -d".")
+    echo "${script_name:-Unknown}" # Default to "Unknown" if empty
 }
 
 get_retval() {
-    retVal=$?
+    # Check the return value of the last command
+    local retVal=$?
     if [ $retVal -ne 0 ]; then
-      echo "Error"
+        echo "Error: Command failed with exit code $retVal"
     fi
     exit $retVal
 }
